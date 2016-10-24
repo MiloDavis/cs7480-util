a2e5799afab8491126f9b38d8e6b29d9999ca895
Milo Davis <davis.mil@husky.neu.edu>
2016-10-24 13:42:40 -0400

    Adds code to find typed racket required paths

--- private/find-required.rkt
+++ private/find-required.rkt
@@ -2,32 +2,46 @@
 
 (provide
  module->typed-identifiers
-  ;; (-> [Path-String] (Listof Identifier))
-  ;; Return a list of identifiers referenced in a module that came
-  ;;  from another module defined with #lang typed/racket or #lang typed/racket/base
+ ;; (-> [Path-String] (Listof Identifier))
+ ;; Return a list of identifiers referenced in a module that came
+ ;;  from another module defined with #lang typed/racket or #lang typed/racket/base
 
  module->required-identifiers
-  ;; (->* [Path-String] [#:only-from (U #f Path-String (Listof Path-String))] (Listof Identifier))
-  ;; Return a list of identifiers referenced in a module that are defined externally.
-  ;; If `#:only-from` is non-#f, the list is filtered to have only identifiers provided
-  ;;  by one of the `#:only-from` modules.
-)
+ ;; (->* [Path-String] [#:only-from (U #f Path-String (Listof Path-String))] (Listof Identifier))
+ ;; Return a list of identifiers referenced in a module that are defined externally.
+ ;; If `#:only-from` is non-#f, the list is filtered to have only identifiers provided
+ ;;  by one of the `#:only-from` modules.
+ module->typed-files
+ )
 
 (require
-  drracket/check-syntax
-  lang-file/read-lang-file
-  racket/class
-  racket/set
-  (only-in racket/path path-only)
-  (only-in racket/string string-prefix?)
-  setup/collects)
+ drracket/check-syntax
+ lang-file/read-lang-file
+ racket/class
+ racket/set
+ (only-in racket/path path-only)
+ (only-in racket/string string-prefix?)
+ setup/collects)
 
 ;; =============================================================================
 
 ;; (-> Path-String Boolean)
 (define current-path-filter (make-parameter (lambda (x) x)))
 
-(define (module->typed-identifiers path-string)
+
+(define (get-required/internal path-string collector-class%)
+  (define annotations (new collector-class%))
+  (define ns (make-base-namespace))
+  (define-values (add-syntax done)
+    (make-traversal ns #f))
+  (parameterize ([current-annotations annotations]
+                 [current-namespace ns]
+                 [current-load-relative-directory (path->complete-path (path-only path-string))])
+    (add-syntax (expand (read-lang-file path-string)))
+    (done))
+  (send annotations collected-data))
+
+(define (module->typed-required/internal path-string collector%)
   (define tr-cache (mutable-set))
   (define (tr-filter p)
     (or (set-member? tr-cache p)
@@ -35,10 +49,34 @@
              (let ([lang (lang-file-lang p)])
                (and lang
                     (string-prefix? lang "typed/racket"))
-                    (set-add! tr-cache p)
-                    #t))))
+               (set-add! tr-cache p)
+               #t))))
   (parameterize ([current-path-filter tr-filter])
-    (get-required/internal path-string)))
+    (get-required/internal path-string collector%)))
+
+(define (module->typed-identifiers path-string)
+  (module->typed-required/internal path-string identifier-collector%))
+
+(define path-collector%
+  (class (annotations-mixin object%)
+    (super-new)
+
+    (define paths
+      (mutable-set))
+
+    (define/public (collected-data)
+      (set->list paths))
+
+    ;; Every non-#f syntax object is interesting
+    (define/override (syncheck:find-source-object stx)
+      stx)
+
+    (define/override (syncheck:add-jump-to-definition source-obj start end id filename submods)
+      (when ((current-path-filter) filename)
+        (set-add! paths filename)))))
+
+(define (module->typed-files path-string)
+  (module->typed-required/internal path-string path-collector%))
 
 (define (module->required-identifiers path-string #:only-from only-from0)
   (define only-from
@@ -50,29 +88,17 @@
   (define (only-from-filter p)
     (member p only-from))
   (parameterize ([current-path-filter only-from-filter])
-    (get-required/internal path-string)))
-
-(define (get-required/internal path-string)
-  (define annotations (new collector%))
-  (define ns (make-base-namespace))
-  (define-values (add-syntax done)
-    (make-traversal ns #f))
-  (parameterize ([current-annotations annotations]
-                 [current-namespace ns]
-                 [current-load-relative-directory (path->complete-path (path-only path-string))])
-    (add-syntax (expand (read-lang-file path-string)))
-    (done))
-  (send annotations collected-identifiers))
+    (get-required/internal path-string identifier-collector%)))
 
 ;; Overrides the `current-annotations` parameter for check-syntax.
-(define collector%
+(define identifier-collector%
   (class (annotations-mixin object%)
     (super-new)
 
     (define ids
       (mutable-set))
 
-    (define/public (collected-identifiers)
+    (define/public (collected-data)
       (set->list ids))
 
     ;; Every non-#f syntax object is interesting
@@ -82,4 +108,3 @@
     (define/override (syncheck:add-jump-to-definition source-obj start end id filename submods)
       (when ((current-path-filter) filename)
         (set-add! ids source-obj)))))
-
